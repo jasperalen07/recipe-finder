281552b747c8159930c2ad7b306aee26653a6ffd
jasperalen07 <125176411+jasperalen07@users.noreply.github.com>
2023-12-21 18:05:55 -0600

    trying to fix the catch

--- recipe-finder project/scripts/index.js
+++ recipe-finder project/scripts/index.js
@@ -10,6 +10,68 @@ searchBtn.addEventListener('click', () => {
     if (userInp.length == 0) {
         result.innerHTML = `<h3>Input Field Cannot be Empty</h3>`
     } else {
+        fetch(url + userInp)
+            .then((response) => response.json())
+            .then(data => {
+                console.log(data)
+                let myMeal = data.meals[0];
+                console.log(myMeal);
+                console.log(myMeal.strMealThumb)
+                console.log(myMeal.strMeal);
+                console.log(myMeal.strArea);
+                console.log(myMeal.strInstructions);
+                let count = 1;
+                let ingredients = []
+                for (let i in myMeal) {
+                    let ingredient = "";
+                    let measure = "";
+                    if (i.startsWith("strIngredient") && myMeal[i]) {
+                        ingredient = myMeal[i];
+                        measure = myMeal[`strMeasure` + count];
+                        count += 1;
+                        ingredients.push(`${measure} ${ingredient}`)
+
+                    }
+                }
+                console.log(ingredients)
+
+                result.innerHTML = `<img src="${myMeal.strMealThumb}"> 
+        <div class="details">
+        <h2>${myMeal.strMeal}</h2>
+        <h4>${myMeal.strArea}</h4>
+        </div>
+        <div id="ingredient-con"></div>
+        <div id="recipe">
+            <button id="hide-recipe">X</button>
+            <pre id="instructions">${myMeal.strInstructions}</pre>
+        </div>
+        <button id="show-recipe">View Recipe<button>
+       `;
+                let ingredientCon = document.getElementById("ingredient-con");
+                let parent = document.createElement("ul");
+                let recipe = document.getElementById("recipe");
+                let hideRecipe = document.getElementById("hide-recipe");
+                let showRecipe = document.getElementById("show-recipe");
+
+                ingredients.forEach((i) => {
+                    let child = document.createElement("li");
+                    child.innerText = i;
+                    parent.appendChild(child);
+                    ingredientCon.appendChild(parent);
+
+                })
+
+
+
+                hideRecipe.addEventListener('click', () => {
+                    recipe.style.display = 'none';
+                })
+
+                showRecipe.addEventListener('click', () => {
+                    recipe.style.display = 'block';
+                })
+
+            });
 
     }
 });
@@ -61,10 +123,11 @@ fetch(url + "big mac")
             let child = document.createElement("li");
             child.innerText = i;
             parent.appendChild(child);
+            ingredientCon.appendChild(parent);
 
         })
 
-        ingredientCon.appendChild(parent);
+
 
         hideRecipe.addEventListener('click', () => {
             recipe.style.display = 'none';
